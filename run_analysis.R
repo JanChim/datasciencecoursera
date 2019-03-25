831f0a660401dffcb83b8f3d3b50f949a8240be4
Janet Chimwayange <47668025+JanChim@users.noreply.github.com>
2019-03-25 20:27:57 +0000

    Cleaning and getting data
    completed peer reviews assignment, wrote script, editted a few times and answered all question including creating anothe data set of averages clean_data.csv

--- run_analysis.R
+++ run_analysis.R
@@ -2,69 +2,57 @@ setwd(dirname(file.choose()))
 getwd()
 
 #see the structure of the data   # The top of the data
-subjecttest.dis <- read.csv("subject_test.csv", stringsAsFactors = FALSE)
-str(subjecttest.dis)
+subject_test <- read.table("subject_test.csv", stringsAsFactors = FALSE)
+str(subject_test)
 
-xtest <- read.csv("x_test.csv", stringsAsFactors = FALSE)
-str(xtest.dis)
+x_test <- read.table("x_test.csv", stringsAsFactors = FALSE)
+str(x_test)
 
-ytest.dis <- read.csv("y_test.csv", stringsAsFactors = FALSE)
-str(ytest.dis)
+y_test <- read.table("y_test.csv", stringsAsFactors = FALSE)
+str(y_test)
 
-subjecttrain.dis <- read.csv("subject_train.csv", stringsAsFactors = FALSE)
-str(subjecttrain.dis)
+subject_train <- read.table("subject_train.csv", stringsAsFactors = FALSE)
+str(subject_train)
 
-xtrain <- read.csv("x_train.csv", stringsAsFactors = FALSE)
-str(xtrain.dis)
+x_train <- read.table("x_train.csv", stringsAsFactors = FALSE)
+str(x_train)
 
-ytrain.dis <- read.csv("y_train.csv", stringsAsFactors = FALSE)
-str(ytrain.dis)
+y_train <- read.table("y_train.csv", stringsAsFactors = FALSE)
+str(y_train)
 
 ## merge train and test set to createone data set
 
-dataSet <- rbind(xtrain,xtest)
+test <- cbind(subject_test,x_test, y_test)
+train <- cbind(subject_train, x_train, y_train)
+merged <- rbind(train, test)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Create a vector of only mean and std, use the vector to subset.
-MeanStdOnly <- grep("mean()|std()", features[, 2]) 
-dataSet <- dataSet[,MeanStdOnly]
+meanstd <- grepl("mean\\(\\)", names(merged)) |
+  grep("std\\(\\)", names(merged))
+meanstd[1:2] <- TRUE
 
-
-# 4. Appropriately labels the data set with descriptive activity names.
-# Create vector of "Clean" feature names by getting rid of "()" apply to the dataSet to rename labels.
-CleanFeatureNames <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
-names(dataSet) <- CleanFeatureNames[MeanStdOnly]
-
-# combine test and train of subject data and activity data, give descriptive lables
-subject <- rbind(subject_train, subject_test)
-names(subject) <- 'subject'
-activity <- rbind(y_train, y_test)
-names(activity) <- 'activity'
-
-# combine subject, activity, and mean and std only data set to create final data set.
-dataSet <- cbind(subject,activity, dataSet)
-
+## to exclude columns that aren't needed
+merged <- merged[, meanstd]
 
 # 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately label data set with descriptive activity names.
 # group the activity column of dataSet, re-name lable of levels with activity_levels, and apply it to dataSet.
-act_group <- factor(dataSet$activity)
-levels(act_group) <- activity_labels[,2]
-dataSet$activity <- act_group
-
+act_group <- factor(merged$activity, 
+                    labels=c("Walking","Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
+levels(act_group)
+merged$activity <- act_group
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-# check if reshape2 package is installed
-if (!"reshape2" %in% installed.packages()) {
-  install.packages("reshape2")
-}
 library("reshape2")
 
-# melt data to tall skinny data and cast means. Finally write the tidy data to the working directory as "tidy_data.txt"
-baseData <- melt(dataSet,(id.vars=c("subject","activity")))
-secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
-names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
-write.table(secondDataSet, "tidy_data.txt", sep = ",")
+# melt data to tall skinny data and cast means. Finally write the tidy data to the working directory as "clean_data.csv"
+averages <- melt(merged, id=c("subjectID", "activity"))
+cleandata <- dcast(averages, subjectID+activity ~ variable, mean)
+
+# write the tidy data set to a file
+write.csv(cleandata, "clean_data.csv", row.names=FALSE)
 
 # remove all variables from the environment
 
